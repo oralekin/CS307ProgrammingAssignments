f2de591d97369892ce6168eaf7e85f7742711c08
Ekin Oral <oralekin@sabanciuniv.edu>
2025-03-04 10:56:02 +0300

    use assignment suggested function for now

--- a1/treePipe.c
+++ a1/treePipe.c
@@ -94,19 +94,23 @@ int doProgram(char *program, int num1, int num2) {
   char* argv[] = {program};
   struct PipedChild child = createPipedChild(program, argv);
   
-  // from assignment spec, strings through pipe are max length 10
-  // when writing to pipe, string should be null terminated.
-  char w_buffer[11];
-  w_buffer[10] = NULL;
+  // // from assignment spec, strings through pipe are max length 10
+  // // when writing to pipe, string should be null terminated.
+  // char w_buffer[11];
+  // w_buffer[10] = NULL;
 
-  // from manpage: writes max n characters into some string
-  snprintf(w_buffer, 10, "%d", num1);
-  if (write(child.tx, w_buffer, 11) == -1) exit(1);
+  // // from manpage: writes max n characters into some string
+  // snprintf(w_buffer, 10, "%d", num1);
+  // if (write(child.tx, w_buffer, 11) == -1) exit(1);
   
-  // same for num2
-  snprintf(w_buffer, 10, "%d", num2);
-  if (write(child.tx, w_buffer, 11) == -1) exit(1);
+  // // same for num2
+  // snprintf(w_buffer, 10, "%d", num2);
+  // if (write(child.tx, w_buffer, 11) == -1) exit(1);
 
+
+  // also from assignment suggestion: use dprintf?
+  dprintf(child.tx, "&%d", num1);
+  dprintf(child.tx, "&%d", num2);
 
   // read 11 bytes because we know by spec that it is max 10
   char r_buffer[11];
